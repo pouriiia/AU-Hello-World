e9270779e1f0ebe8c1602dd497a3c5a7aa757983
pouriiia <cloner.1383@gmail.com>
2025-11-14 21:56:30 +0330

    Add code sample

---
+++ PHP/Conditions.php
@@ -0,0 +1,14 @@
+<?php
+
+$isStudent = true;
+$isTeacher = false;
+
+if ($isStudent) {
+    echo "You are a student.\n";
+}
+
+if ($isTeacher) {
+    echo "You are a teacher.";
+} else {
+    echo "You are NOT a teacher.";
+}
